35bfd04be5ba25bc675c1c954bf0d5bfd77d907e
M1TKO <mitko.kalenderov@gmail.com>
2017-02-08 22:22:45 +0200

    Big changes in the design and fixed some bugs

--- enc_dec.php
+++ enc_dec.php
@@ -1,20 +1,22 @@
 <?php 
-$errorKey = $errorData = $dataSet = $result = "";
+$errorKey = $errorData = $dataSet = $result = $showKey = "";
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$operation = $_POST['operation'];
 	$dataSet = true;
 
 	if(empty($_POST['key'])){
 		$dataSet = false;
-		$errorKey = "You have to enter at least 1 symbol. ";
+		$errorKey = "* You have to enter at least 1 symbol! ";
 		$key = "";
+		echo "<style>.errorMsg1{display:block;}</style>";
 	}else{
 		$key = $_POST['key'];
 	}
 	if(empty($_POST['data'])){
 		$dataSet = false;
-		$errorData = "You have to enter a data for encryption"; 
+		$errorData = "* You have to enter a data for encryption!"; 
 		$data = "";
+		echo "<style>.errorMsg2{display:block;}</style>";
 	}else{
 		$data = $_POST['data'];
 	}
@@ -29,15 +31,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			$enc = $aes->encrypt();
 			$aes->setData($enc);
 			$result = $enc;
+			$showKey = $key;
 		} elseif ($operation == "decrypt" && $dataSet === true) {
 			// decription code here
 			$dec = $aes->decrypt();
 			$result = $dec;
 	    }
 	}
-	echo "type:  $operation <br>";
-	echo "key:  $key<br>";
-	echo "data:  $data <br>";
-	echo "bits:  $encBit<br><br>";
 }
 ?>
--- index.php
+++ index.php
@@ -1,34 +1,70 @@
 <?php require_once 'AES.php';
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<title>AES encryption</title>
+	<link rel="icon" href="media/icon.png">
+	<link rel="stylesheet" href="style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<style>
+		.errorMsg1, .errorMsg2{display: none;}
+	</style>
 </head>
 <body>
 <?php include 'enc_dec.php'; ?>
+<p id="header">AES encryption online</p>
+<div class="main-container">
+	<form method="POST" action="<?php htmlspecialchars($_SERVER['PHP_SELF']);?>">
 
-<form method="POST" action="<?php htmlspecialchars($_SERVER['PHP_SELF']);?>">
+		<p>Enter encryption key:</p>
+		<span class="errorMsg1"><?php echo $errorKey; ?></span>
+		<input type="text" name="key" id="key" maxlength="100">
+		
+		<p>Enter your data for encryption / decryption:</p>
+		<span class="errorMsg2"><?php echo $errorData; ?></span>
+		<textarea name="data" id="data"  rows="15"></textarea><br>
 
-	<p>Enter encryption key:</p>
-	<input type="text" name="key" id="key">
-	<span><?php echo $errorKey; ?></span>
-	<p>Enter your data for encryption</p>
-	<textarea name="data" id="data" cols="80" rows="15" id="data"></textarea><br>
-	<span><?php echo $errorData; ?></span>
-	<p>Choose the level of encryption</p>
- 	<label>128 Bit<input type="radio" name="encBit" value="128" checked></label><br>
- 	<label>192 Bit<input type="radio" name="encBit" value="192"></label><br>
- 	<label>256 Bit<input type="radio" name="encBit" value="256"></label><br>
- 	<br>
- 	<input name="operation" type="submit" value="encrypt" id="submitBtn">
- 	<input name="operation" type="submit" value="decrypt" id="submitBtn">
-</form>
+		<div class="radio-btns">
+			<p>Choose the level of encryption:</p>
+	 		<input type="radio" name="encBit" value="128" id="rb1" checked>
+	 		<label for="rb1">128 Bit</label>
+	 		<input type="radio" name="encBit" value="192" id="rb2">
+	 		<label for="rb2">192 Bit</label>
+	 		<input type="radio" name="encBit" value="256" id="rb3">
+	 		<label for="rb3">256 Bit</label>
+	 	</div>
+	 	<br>
 
-<div class="show-result">
-	<span id=result><?php echo $result; ?></span>
+	 	<input name="operation" type="submit" value="encrypt" id="submitBtn">
+	 	<input name="operation" type="submit" value="decrypt" id="submitBtn">
+	</form>
+
+
+	<div class="show-result">
+			<div class="showKey">
+				<p>KEY </p>
+				<button id="copyKey" onclick="copyToClipboard('#keyShow')">
+					<img src="media/copy.svg" alt="copy">
+				</button>
+				<textarea readonly="readonly" id="keyShow" rols="1" cols="1"><?php echo $showKey;?></textarea>
+			</div>
+		<textarea readonly="readonly" cols="80" rows="15" id="result"><?php echo $result; ?></textarea>
+		<button id="copyData" onclick="copyToClipboard('#result')">Copy to clipboard <img src="media/copy.svg" alt="copy">
+		</button>
+	</div>
+</div>
+<div class="footer">
+	<p>Programmed and styled by Dimitar Kalenderov.</p>
+	<a href="https://github.com/M1TKO/AES-encryption-website" target="_blank">
+		<img src="media/github.png" alt="github">
+	</a>
+	<a href="http://aesencryption.net/" target="_blank">
+		<img src="media/aes.jpg" alt="aes">
+	</a>
+	<p>Big thanks to aesencription.net for AES algorithm. <3</p>
 </div>
 
-
+<script type="text/javascript" src="script.js"></script>
 </body>
 </html>
